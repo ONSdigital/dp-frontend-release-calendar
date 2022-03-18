45b8d5691067928b6b9677a22c3f1e9f40d32d9e
Giles Burdett <giles@cogniworks.co.uk>
2022-03-18 16:39:28 +0000

    Replace inline styling with dp-design-system update

--- config/config.go
+++ config/config.go
@@ -33,7 +33,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/f34f3ac"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/42275e1"
 	}
 
 	return cfg, nil
