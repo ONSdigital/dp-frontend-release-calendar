c365c66c830037a273b54e73b4060358cbc107c6
Yomi Odeleye <odeleye.yomi@me.com>
2022-03-30 04:34:24 -0400

    fix indentation of mapper

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"time"
 
+	"github.com/ONSdigital/dp-api-clients-go/v2/releasecalendar"
 	"github.com/ONSdigital/dp-frontend-release-calendar/config"
 	"github.com/ONSdigital/dp-frontend-release-calendar/model"
+	"github.com/ONSdigital/dp-renderer/helper"
 	coreModel "github.com/ONSdigital/dp-renderer/model"
-    "github.com/ONSdigital/dp-renderer/helper"
-	"github.com/ONSdigital/dp-api-clients-go/v2/releasecalendar"
 )
 
 func createTableOfContents(
@@ -253,7 +253,7 @@ func CreateCalendar(_ context.Context, basePage coreModel.Page, _ config.Config)
 		Page: basePage,
 	}
 	calendar.BetaBannerEnabled = true
-    calendar.Metadata.Title = helper.Localise("ReleaseCalendarPageTitle", calendar.Language, 1)
+	calendar.Metadata.Title = helper.Localise("ReleaseCalendarPageTitle", calendar.Language, 1)
 
 	item1 := model.CalendarItem{
 		URI: "/releases/title1",
