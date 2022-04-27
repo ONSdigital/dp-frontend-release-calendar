9e3dfc192a95062c87f7ec71bcec696827af2949
Giles Burdett <giles@cogniworks.co.uk>
2022-04-27 10:35:37 +0100

    Release type filter radios auto-submit on change

--- config/config.go
+++ config/config.go
@@ -39,7 +39,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/7aa530a"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/5f45c7f"
 	}
 
 	return cfg, nil
