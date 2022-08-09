8de4a0af5f04778595c4fa4088dfac3c9eaf9245
Yomi Odeleye <odeleye.yomi@me.com>
2022-08-09 11:21:26 +0100

    point dp-design system to new hash

--- config/config.go
+++ config/config.go
@@ -43,7 +43,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/812f050"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/1c9b541"
 	}
 
 	cfg.RoutingPrefix = validateRoutingPrefix(cfg.RoutingPrefix)
