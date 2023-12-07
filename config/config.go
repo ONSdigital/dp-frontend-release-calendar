5a720bbcf9bf4cefbedabbc0e92ce37ca530ab42
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2023-12-07 12:55:11 +0000

    1895 update design system commit hash

--- config/config.go
+++ config/config.go
@@ -43,7 +43,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/e0a75c3"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/00353da"
 	}
 
 	cfg.RoutingPrefix = validateRoutingPrefix(cfg.RoutingPrefix)
