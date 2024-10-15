352488f5b823cd974d1f8aa9d15c01b0539ee6e6
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2024-10-15 10:35:01 +0100

    Fix linter issues

--- assets/assets.go
+++ assets/assets.go
@@ -1,3 +1,12 @@
 // assets in locale and templates folders are converted into Go source code with go-bindata
 // the data.go file in this package is auto generated through the generate-debug and generate-prod tasks in the Makefile
 package assets
+
+// these two functions stop a 'could not import' lint error
+func Asset(_ string) ([]byte, error) {
+	return nil, nil
+}
+
+func AssetNames() []string {
+	return []string{}
+}
